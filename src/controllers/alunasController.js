0dd37634eb1c1bddee8a68f0e857147e94698aaf
joyceeazevedo <azevedoeferreira@gmail.com>
2019-10-08 20:03:32 -0300

    subindo api

---
+++ src/controllers/alunasController.js
@@ -0,0 +1,13 @@
+const alunas = require ("../model/alunas.json")
+
+exports.get = (req,res) => {
+ console.log (req.url)
+ res.status(200).send(alunas)
+}
+
+exports.getById = (req, res) => {
+  const id = req.params.id
+  console.log(id)
+  res.status(200).send(alunas.find(alunas => alunas.id == id))
+
+}
